9bc8f67fb06e0367cc79ec28bf4b99d67196f137
Gabriel Neto <gabrielsebastiao321@gmail.com>
2024-12-03 16:26:30 -0300

    adicionando o projeto do boberto para o arduino uno e removendo o codigo do mega

--- RoboBobertoUno/src/main.cpp
+++ RoboBobertoUno/src/main.cpp
@@ -6,11 +6,13 @@
 Robot boberto;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
     boberto.begin();
     Serial.println(":: Oi, eu sou o Boberto ::");
 }
 
 void loop() {
   boberto.forward();
+  Serial.println("Boberto aqui 2");
+  delay(100);
 }
